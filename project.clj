ce9bf5ecef5126589a05d6e296939ffdcb6270b2
Michael Klishin <michael@defprotocol.org>
2013-03-21 18:48:39 +0400

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject clojurewerkz/serialism "1.1.0-SNAPSHOT"
   :description "A tiny Clojure library that serializes and deserializes values into popular formats based on provided content type"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojurewerkz/support "0.7.0"]
-                 [cheshire             "4.0.3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojurewerkz/support "0.15.0"]
+                 [cheshire             "5.0.2"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
-                   :plugins [[codox "0.6.1"]]
+                   :plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
