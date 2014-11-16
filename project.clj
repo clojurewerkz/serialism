5bb7d441b0a3735d2f224335b892a55a6d3c7c92
Michael Klishin <michael@clojurewerkz.org>
2014-11-16 05:35:02 +0300

    Test against 1.7, drop 1.4

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject clojurewerkz/serialism "1.3.0-SNAPSHOT"
   :description "A tiny Clojure library that serializes and deserializes values into popular formats based on provided content type"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure  "1.6.0"]
                  [clojurewerkz/support "1.1.0"]
                  [cheshire             "5.3.1"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
